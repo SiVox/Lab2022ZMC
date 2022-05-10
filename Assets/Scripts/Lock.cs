8942367a461b997c17399beaa1d6122df2d14df1
MateuszUjma <91689578+SiVox@users.noreply.github.com>
2022-05-10 18:22:54 +0200

    10.05.2022

---
+++ Assets/Scripts/Lock.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    public Door[] doors;
+    public KeyColor myColor;
+    bool
+        locked = false,
+        iCanOpen = false;
+
+    Animator key;
+
+
+    
+    void Start()
+    {
+        key = GetComponent<Animator>();
+    }  
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            iCanOpen = true;
+            Debug.Log("you can use");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            iCanOpen = false;
+            Debug.Log("you can`t use");
+        }
+    }
+
+    public void UseKey()
+    {
+        foreach(Door door in doors)
+        {
+            door.OpenClose();
+        }
+    }
+
+    public bool CheckTheKey()
+    {
+        if (GameManager.gameManager.redKey > 0 && myColor == KeyColor.Red) 
+        {
+            GameManager.gameManager.redKey--;
+            locked = true;
+            return true;
+        }
+        else if(GameManager.gameManager.greenKey > 0 && myColor == KeyColor.Green)
+        {
+            GameManager.gameManager.greenKey--;
+            locked = true;
+            return true;
+        }
+        else if (GameManager.gameManager.goldKey > 0 && myColor == KeyColor.Gold)
+        {
+            GameManager.gameManager.goldKey--;
+            locked = true;
+            return true;
+        }
+        else
+        {
+            Debug.Log("Nie masz kluczy");
+            return false;
+        }
+    }
+
+}
